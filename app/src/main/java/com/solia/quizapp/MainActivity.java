6dc98eab7a63c19848a5f6c8852d37574bf2ca17
Sergey Provorov <s.provorov@triogames.net>
2023-02-20 05:38:47 +0300

    edit text added

--- app/src/main/java/com/solia/quizapp/MainActivity.java
+++ app/src/main/java/com/solia/quizapp/MainActivity.java
@@ -1,13 +1,28 @@
 package com.solia.quizapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.wellcome_screen);
+
+        textView = findViewById(R.id.welcome_text);
+        editText = findViewById(R.id.input_name_field);
+        button = findViewById(R.id.confirm_button);
+
+        button.setOnClickListener(v -> textView.setText("Welcome " + editText.getText()));
     }
 }
